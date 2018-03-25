ba6e1aa9af89958b3136b9e4fac120c21f6e06f6
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-25 13:40:38 -0700

    fixed login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,41 +1,11 @@
-import React, { Component } from 'react';
-import {
-  Dimensions,
-  ScrollView,
-  StyleSheet,
-  Text,
-  Button,
-  View,
-  Input, ImageBackground } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text, Button, ImageBackground } from 'react-native';
+import Expo from 'expo';
 
-import { Expo, Font } from 'expo';
-import Styled from 'styled-components/native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-
-const SCREEN_WIDTH = Dimensions.get('window').width;
-const SCREEN_HEIGHT = Dimensions.get('window').height;
-
-const BG_IMAGE = require('../assets/images/bg_screen1.jpg');
-
-export default class LoginScreen extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      fontLoaded: false,
-    };
-  }
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      'georgia': require('../assets/fonts/Georgia.ttf'),
-      'regular': require('../assets/fonts/Montserrat-Regular.ttf'),
-      'light': require('../assets/fonts/Montserrat-Light.ttf'),
-      'bold': require('../assets/fonts/Montserrat-Bold.ttf'),
-    });
-
-    this.setState({ fontLoaded: true });
-  }
+export default class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Login',
+  };
 
   async signInWithGoogleAsync() {
   try {
@@ -46,6 +16,7 @@ export default class LoginScreen extends Component {
     });
 
     if (result.type === 'success') {
+      this.props.navigation.navigate('Main');
       return result.accessToken;
     } else {
       return { cancelled: true };
@@ -56,103 +27,35 @@ export default class LoginScreen extends Component {
 }
 
   render() {
-
     return (
-      <View style={styles.container}>
-        <ImageBackground
-          source={BG_IMAGE}
-          style={styles.bgImage}
-        >
-          {this.state.fontLoaded ?
-            <View style={styles.loginView}>
-              <View style={styles.loginTitle}>
-                <View style={{ flexDirection: 'row' }}>
-                  <Text style={styles.travelText}>Therify</Text>
-                </View>
-              </View>
-              <View style={styles.loginInput}>
-                
-              </View>
-              <View style={styles.loginButton}>
-                <Button
-                  title='SIGN IN WITH GOOGLE'
-                  activeOpacity={1}
-                  underlayColor="transparent"
-                  onPress={this.signInWithGoogleAsync.bind(this)}
-                  loadingProps={{ size: 'small', color: 'white' }}
-                  buttonStyle={{ height: 50, width: 250, backgroundColor: 'transparent', borderWidth: 2, borderColor: 'white', borderRadius: 30 }}
-                  containerStyle={{ marginVertical: 10 }}
-                  titleStyle={{ fontWeight: 'bold', color: '#FFFFFF' }}
-                />
-              </View>
-              <View style={styles.footerView}>
-                <Text style={{ color: 'grey' }}>
-                  New here?
-              </Text>
-                <Button
-                  title="Create an Account"
-                  clear
-                  activeOpacity={0.5}
-                  titleStyle={{ color: 'white', fontSize: 15 }}
-                  containerStyle={{ marginTop: -10 }}
-                  onPress={() => console.log('Account created')}
-                />
-              </View>
-            </View> :
-            <Text>Loading...</Text>
-          }
-        </ImageBackground>
-      </View>
+      <ImageBackground
+        source={require('../assets/images/bg_screen1.jpg')}
+        style={{
+          flex: 1,
+          width: null,
+          height: null,
+        }}
+      >
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Button
+            onPress={ this.signInWithGoogleAsync.bind(this)}
+            title="Sign in with Google" 
+          />
+        </View>
+
+      </ImageBackground >
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  },
-  bgImage: {
     flex: 1,
-    top: 0,
-    left: 0,
-    width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
-    justifyContent: 'center',
-    alignItems: 'center'
+    paddingTop: 15,
+    backgroundColor: '#fff'
   },
-  loginView: {
-    marginTop: 150,
-    backgroundColor: 'transparent',
-    width: 250,
-    height: 350,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  loginTitle: {
+  backgroundImage: {
     flex: 1,
-  },
-  travelText: {
-    color: 'white',
-    fontSize: 30,
-    fontFamily: 'bold'
-  },
-  plusText: {
-    color: 'white',
-    fontSize: 30,
-    fontFamily: 'regular'
-  },
-  loginInput: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  loginButton: {
-    flex: 1,
-  },
-  footerView: {
-    marginTop: 20,
-    flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
+    resizeMode: 'cover'
   }
 });
