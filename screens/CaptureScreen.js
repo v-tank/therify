8181b5f0a0ed7791860f0cc4fa98f019e64317bd
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-25 15:44:26 -0700

    modified file names and removed unnecessary code

--- screens/CaptureScreen.js
+++ screens/CaptureScreen.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
-export default class LinksScreen extends React.Component {
+export default class CaptureScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'Capture',
   };
 
   render() {
