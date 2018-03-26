4495fac266f2d6489b10afe3ed279ab641d5bc9f
nwactor <nickwactor@gmail.com>
2018-03-25 20:13:48 -0700

    preliminary mongo models

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	//should users be able to see a list of their photos? Probably.
+	//can mongoose handle many to one relationships where we want to be
+	//able to access the data from either side?
+	photos: { type: [Schema.Types.ObjectId], ref: "Photo"}
+	
+	//comments?
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports User;
