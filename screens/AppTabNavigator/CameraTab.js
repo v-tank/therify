6f806c8348d1bc7e4e08bdc8bcf6439cde5181de
hmalle <hmalle90ak@gmail.com>
2018-03-28 14:23:05 -0700

    the camera code

--- screens/AppTabNavigator/CameraTab.js
+++ screens/AppTabNavigator/CameraTab.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,TouchableOpacity} from 'react-native';
 import { Icon } from 'native-base';
 import { Camera,Permissions} from 'expo';
 
@@ -12,26 +12,58 @@ class CameraTab extends Component {
       <Icon name="ios-camera" style={{ color: tintColor }} />
     )
   }
-  
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>CameraTab</Text>
-      </View>
-    );
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
   }
-}
 
-// define your styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-});
+  snap(){
+    if(this.camera){
+      this.camera.takePictureAsync().then(function(){
+        console.log("a Picture has been taken");
+      });
+    }
+  render() {
+    const { hasCameraPermission } = this.state;
+      if (hasCameraPermission === null) {
+        return <View />;
+      } else if (hasCameraPermission === false) {
+        return <Text>No access to camera</Text>;
+      } else {
+        return (
+          <View style={{ flex: 1 }}>
+            <Camera style={{ flex: 3 }} type={this.state.type}>
+              <View style={{flex: 1, backgroundColor: 'transparent', flexDirection: 'row',}}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                    ?Camera.Constants.Type.front:Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+};
 
 //make this component available to the app
 export default CameraTab;
