9fa5e1151259468b9ac6a8cc23e907cfe4f344c3
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-29 09:50:50 -0700

    working maps for android and ios

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Constants, MapView, Location, Permissions} from 'expo';
+import { StyleSheet } from 'react-native';
+import { MapView, Location, Permissions} from 'expo';
 
 class MapComponent extends Component {
   state = {
-    locationResults:null,
-    mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }
+    mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
+    locationResult: null,
+    location: { coords: { latitude: 37.78825, longitude: -122.4324 } },
   };
   
   componentDidMount() {
-    //when the component mount get the current location from the device and center that on the maps
     this._getLocationAsync();
   }
 
@@ -18,30 +18,21 @@ class MapComponent extends Component {
     if (status !== 'granted') {
       this.setState({
         locationResult: 'Permission to access location was denied',
+        location,
       });
     }
-    let loc = await Location.getCurrentPositionAsync({}); 
-    let newLocation={
-      latitude: loc.coords.latitude,
-      longitude: loc.coords.longitude,
-      latitudeDelta: 0.0922, 
-      longitudeDelta: 0.0421 
-    }
-    this.setState({mapRegion:newLocation});
-  };
 
-  _handleMapRegionChange = mapRegion => {
-    this.setState({ mapRegion });
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ locationResult: JSON.stringify(location), location, });
   };
 
   render() {
     return (
       <MapView
+        provider={'google'}
         style={{ alignSelf: 'stretch', height: 200 }}
-        region={this.state.mapRegion}
-        provider={MapView.PROVIDER_GOOGLE}
-        onRegionChange={this._handleMapRegionChange}
-        showUserLocation = {true}
+        showsUserLocation={true}
+        region={{ latitude: this.state.location.coords.latitude, longitude: this.state.location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
       />
     );
   }
