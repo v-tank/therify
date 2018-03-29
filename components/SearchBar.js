ded4d9d217e62651f0446f3ffb31965114d55873
Your Name <you@example.com>
2018-03-29 14:09:59 -0700

    resolving issues

---
+++ components/SearchBar.js
@@ -0,0 +1,44 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+// create a component
+class SearchBar extends Component {
+
+  state = {
+    locationText: '',
+  };
+
+  searchPressed = () => {
+    const { locationText } = this.state;
+    alert(`Searching for ${locationText}`);
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          ref="search"
+          placeholder="Search"
+          style={{ height: 40, fontSize: 20, textAlign: 'center' }}
+          onChangeText={locationText => this.setState({ locationText })}
+        />
+
+        <Button title="Search" onPress={this.searchPressed} />
+      </View>
+    );
+  }
+}
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+});
+
+//make this component available to the app
+export default SearchBar;
