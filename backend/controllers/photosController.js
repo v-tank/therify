1223b6f8c5b251e75cb7b93d92a22907f4366395
nwactor <nickwactor@gmail.com>
2018-03-29 10:31:38 -0700

    controller for adding photos to DB

--- backend/controllers/photosController.js
+++ backend/controllers/photosController.js
@@ -2,12 +2,15 @@ const db = require("../models");
 
 module.exports = {
 	add: function(req, res) { //add a photo to the db
+		db.Photo
+			.create(req.body)
+			.then(createdPhoto => res.json(createdPhoto))
+			.catch(err => res.status(422).json(err));
+	},
+	getWithComments: function(req, res) { //get a specific photo and its comments
 		
 	},
-	getWithComments: function(req, res) {
-
-	},
-	remove: function(req, res) {
+	remove: function(req, res) { //also remove associated comments
 
 	},
 	findByLocation: function(req, res) {
