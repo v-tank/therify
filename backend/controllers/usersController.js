3678b4cf37f77b93bc98c7723b3ba7fc9fcb6f94
nwactor <nickwactor@gmail.com>
2018-03-29 12:27:25 -0700

    backend code for adding and retrieving users

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -2,12 +2,17 @@ const db = require("../models");
 
 module.exports = {
 	add: function(req, res) { //add a user to the db
-
+		db.User
+			.create(req.body)
+			.then(createdUser => res.json(createdUser))
+			.catch(err => res.status(422).json(err));
+	},
+	get: function(req, res) {
+		db.User.findOne({email: req.body.email})
+			.then(foundUser => res.json(foundUser))
+			.catch(err => res.status(422).json(err));
 	},
 	getPhotos: function(req, res) {
-
-	},
-	addPhoto: function(req, res) {
 
 	}
 };
