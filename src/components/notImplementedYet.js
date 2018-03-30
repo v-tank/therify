69734a94fb9855bdd73e2ad4a57cc0a6f6c7da97
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-30 11:57:26 -0700

    deleted unnecessary files

--- src/components/notImplementedYet.js
+++
@@ -1,3 +0,0 @@
-// @flow
-// eslint-disable-next-line no-alert
-export const notImplementedYet = () => alert("Not Implemented 🤷🏻‍♂️");
--- src/social/components/index.js
+++
@@ -1,8 +0,0 @@
-// @flow
-export {default as Post} from "./Post";
-export {Stories} from "./stories";
-export {default as Comments} from "./Comments";
-export {default as Message} from "./Message";
-export {default as NewMessage} from "./NewMessage";
-export {default as Handle} from "./Handle";
-export {default as ChatMessage} from "./ChatMessage";
