ddc03b06519dd29b924a89355f1e3593050d242e
nwactor <nickwactor@gmail.com>
2018-03-29 19:15:41 -0700

    app reaches server from login

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,23 +1,28 @@
 const db = require("../models");
 
 module.exports = {
-<<<<<<< HEAD
+	// get: function(req, res) {
+	// 	db.User.findOne({email: req.body.email})
+	// 		.then(foundUser => res.json(foundUser))
+	// 		.catch(err => res.status(422).json(err));
+	// },
 	login: function(req, res) {
-		
-	},
-=======
-
->>>>>>> 71afac1734f9f128a0ea6cefc642ff9f6340a405
-	add: function(req, res) { //add a user to the db
+		console.log(req.body);
 		db.User
-			.insertOne(req.body)
-			.then(createdUser => res.json(createdUser))
-			.catch(err => res.status(422).json(err));
-	},
-	get: function(req, res) {
-		db.User.findOne({email: req.body.email})
-			.then(foundUser => res.json(foundUser))
-			.catch(err => res.status(422).json(err));
+			.create(req.body)
+			.then(result => {
+				if(result.acknowledged) { //the user was new
+					console.log("New user registered.");
+					db.User.findById(result.id)
+						.then(newUser => res.json(newUser))
+						.catch(err => res.status(422).json(err));
+				} else { //find and return the user
+					console.log("Existing user returning");
+					db.User.findOne({email: req.body.email})
+						.then(foundUser => res.json(foundUser))
+						.catch(err => res.status(422).json(err));
+				}
+			}).catch(err => res.status(422).json(err));
 	},
 	getPhotos: function(req, res) {
 
