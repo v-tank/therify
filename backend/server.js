b22967153f9630e55d3f7897d8b64192615d536b
nwactor <nickwactor@gmail.com>
2018-04-02 22:25:01 -0700

    fixed duplicate server code

--- backend/server.js
+++ backend/server.js
@@ -110,31 +110,4 @@ function global_dist(pos1, pos2, range){
         // console.log(false);
 		return false;
 	}
-}var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-
-var db = require("./models");
-var controllers = require("./controllers");
-
-var PORT = process.env.PORT || 8080;
-
-var app = express();
-
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json({limit: '10mb'})); //for some reason, this is needed to see the body from fetch requests
-
-var routes = require("./routes/routes");
-app.use("/", routes);
-
-mongoose.Promise = Promise; //Set Mongo to use promises for asynch queries
-
-//connect to db
-mongoose.connect("mongodb://localhost/therify", {
-  //useMongoClient: true //got a warning saying to remove this
-}).then(response => console.log("Connected to server."));
-
-//start server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});
+}
