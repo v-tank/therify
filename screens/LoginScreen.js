19fdb35ed4743fa62b7521927ad0dc7545cc9fd5
nwactor <nickwactor@gmail.com>
2018-04-03 22:45:11 -0700

    connecting to heroku servergit add .!

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -23,7 +23,7 @@ export default class LoginScreen extends Component {
 
         var serverRequest = { email: result.user.email };
 
-        fetch('http://10.142.85.95:8080/user/login', {
+        fetch('https://therifyserver.herokuapp.com/user/login', {
           method: 'POST',
           body: JSON.stringify(serverRequest),
           headers: {
