a0ba393a6788cb6a9d4dab262cbbd3f5a47f231a
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-04-03 23:16:02 -0700

    updated IP

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -23,7 +23,7 @@ export default class LoginScreen extends Component {
 
         var serverRequest = { email: result.user.email };
 
-        fetch('http://10.142.85.95:8080/user/login', {
+        fetch('http://192.168.0.12:8080/user/login', {
           method: 'POST',
           body: JSON.stringify(serverRequest),
           headers: {
