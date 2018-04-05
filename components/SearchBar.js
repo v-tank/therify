c09b15f4fce0d14a7c63dea88f52a7b7eb92dbcf
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-04-05 16:37:19 -0700

    cleaned up code and added comments

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,4 +1,4 @@
-//import liraries
+// import components
 import React, { Component } from 'react';
 import { ActivityIndicator, View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { Permissions, Location } from 'expo';
@@ -7,15 +7,17 @@ import { Feather, FontAwesome as Icon } from "@expo/vector-icons";
 // create a component
 class SearchBar extends Component {
 
+  // updates the state as the user types in the location
   handleTextInputChange = (text) => {
-    // console.log(text)
     this.props.updateState(text);
   }
 
+  // calls the updateLocation function passed in as a prop
   handleSubmit = () => {
     this.props.updateLocation();
   }
 
+  // Renders the search bar's text input and search icon
   render() {
     return (
       <View style={{ flexDirection: 'row', margin: 10, justifyContent: 'space-around', alignItems: 'center' }}>
